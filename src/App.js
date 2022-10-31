92e584fa6b2e0a5b28fd36afa5d414a4e449be48
DMould123 <95115610+DMould123@users.noreply.github.com>
2022-10-31 12:49:05 +0100

    Added new image and text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import image from './image.jpg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={image} className="App-image" alt="" />
+        <p>BK Häcken 2022</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="App-image"
+          href="https://bkhacken.se/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SM Guld
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
